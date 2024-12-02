38d350607c9c5333e9079e5738be6cdf4995b763
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-12-01 22:04:49 -0800

    Solves day 2 part 1

--- days/d02/main.go
+++ days/d02/main.go
@@ -2,33 +2,63 @@ package main
 
 import (
 	"aoc24"
+	"log/slog"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
 func main() {
+  slog.Info("Loading input")
   in := aoc24.GetInput(2)
 
+  slog.Info("Parsing input")
+  var count int
+  for _, level := range strings.Split(string(in), "\n") {
+    if level == "" {
+      continue
+    }
+    if isLevelSafe(parseLevel([]byte(level))) {
+      count++
+    }
+  }
+  slog.Info("Done", "answer", count)
 }
 
 func isLevelSafe(level []int) bool {
+  var dir int
   for i, n := range level {
     if i == 0 {
       continue
     }
     m := level[i-1]
-    d := dist(n, m)
+    d, r := distAndDirection(n, m)
+    
+    // No change is not safe
+    if d == 0 {
+      return false
+    }
+
+    // Different direction is not safe
+    if dir != 0 && dir != r {
+      return false
+    }
+    dir = r
+
+    // Too big a step is not safe
+    if d > 3 {
+      return false
+    }
   }
   return true
 }
 
-func dist(a, b int) int {
+func distAndDirection(a, b int) (int, int) {
   d := b - a
   if d < 0 {
-    return -d
+    return -d, -1
   }
-  return d
+  return d, 1
 }
 
 func parseLevel(in []byte) []int {
